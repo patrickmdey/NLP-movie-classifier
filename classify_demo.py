3edcae61743234c720874e8190d4f8216d2b9888
patrickmdey <patrickmdey@hotmail.com>
2023-11-12 20:34:04 -0300

    refactor: classify_demo

--- classify_demo.py
+++ classify_demo.py
@@ -25,39 +25,45 @@ def classify(model, prompt, device, tokenizer):
 
 if __name__ == "__main__":
 
-    checkpoint_model = "bert"
-
-    if checkpoint_model == "bert":
-        checkpoint = "distilbert-base-uncased"
-        model_name = "Patrickmdey/nlp-imdb80-distilbert"
-    else:
-        checkpoint = "roberta-base"
-        model_name = "Patrickmdey/nlp-roberta-base"
-
-    
-    
-    device = "cpu"
-
-    logger.info("Loading tokenizer")
-    # Load the tokenizer and tokenize the dataset
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-    tokenizer.add_special_tokens({"pad_token": "[PAD]"})
-
-    logger.info("Loading BERT model")
-    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=5, pad_token_id=tokenizer.pad_token_id).to(device)
-
-    # """
-    # Classify an entire dataset
-    # """
-    # df = pd.read_csv("out/generator/joined_movies_preprocessed/generated_reviews_classified.csv", sep=";")
-    # logger.info("Classifying prompt")
-    # df["predicted"] = df["review"].apply(lambda x: classify(model, x, device=device, tokenizer=tokenizer))
-    # df = df[['predicted', 'rating', 'review']]
-    # df.to_csv("out/generator/joined_movies_preprocessed/generated_reviews_classified.csv", sep=";", index=False)
-
-    """
-    Classify a single prompt
-    """
+    model_type = "bert"
     prompt="I thought this was a 'great' film! It was funny at times and very funny at others, but still got a long way to go! However, I think this is still one of the few films in my collection of films that makes up for the short film running time. I think the film is very well thought out, it\'s very well set up, it\'s very well acted and has very memorable characters. The special effects are definitely top notch - the aliens are"
-    result = classify(model, prompt, device=device, tokenizer=tokenizer)
-    print("Rating:", result)
+
+    if model_type == "roberta":
+
+        checkpoint = "roberta-base"
+        device = "cuda"
+
+        logger.info("Loading tokenizer")
+        # Load the tokenizer and tokenize the dataset
+        tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+        token_id = 1
+
+        logger.info("Loading BERT model")
+        model_path = "out/roberta/saved-models/imdb80-roberta"
+        model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=5, pad_token_id=token_id).to(device)
+
+        """
+        Classify a single prompt
+        """
+        result = classify(model, prompt, device=device, tokenizer=tokenizer)
+        print("Rating:", result)
+    else:
+        #TODO: device cuda for usage in pampero or cuda
+        checkpoint = "distilbert-base-uncased"
+        device = "cuda"
+
+        logger.info("Loading tokenizer")
+        # Load the tokenizer and tokenize the dataset
+        tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+        tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+        token_id = tokenizer.pad_token_id
+
+        logger.info("Loading BERT model")
+        model_path = "out/bert/saved-models/imdb80-distilbert2"
+        model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=5, pad_token_id=token_id).to(device)
+
+        """
+        Classify a single prompt
+        """
+        result = classify(model, prompt, device=device, tokenizer=tokenizer)
+        print("Rating:", result)
